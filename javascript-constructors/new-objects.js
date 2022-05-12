1909bedf9dda68ad35e9fbd4730c19d98efe838f
Michael Chang <michaelchang.kuo@gmail.com>
2022-05-12 22:58:15 +0000

    finished javascript-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,23 @@
+var AnObject = new Object();
+console.log('value of AnObject', AnObject);
+console.log('typeof of AnObject', typeof AnObject);
+
+var AnArray = new Array();
+console.log('value of AnArray', AnArray);
+console.log('typeof of AnArray', typeof AnArray);
+
+var AnFunction = new Function();
+console.log('value of AnFunction', AnFunction);
+console.log('typeof of AnFunction', typeof AnFunction);
+
+var AnString = new String();
+console.log('value of AnString', AnString);
+console.log('typeof of AnString', typeof AnString);
+
+var AnNumber = new Number();
+console.log('value of AnNumber', AnNumber);
+console.log('typeof of AnNumber', typeof AnNumber);
+
+var AnBoolean = new Boolean();
+console.log('value of AnBoolean', AnBoolean);
+console.log('typeof of AnBoolean', typeof AnBoolean);
