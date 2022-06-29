4985617ae5b53330dc60a6ac13a9c803e3a685dd
Michael Chang <michaelchang.kuo@gmail.com>
2022-06-29 03:56:11 +0000

    finished user can add note

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -2,15 +2,36 @@
 const fs = require('fs');
 
 const answer = process.argv[2];
+const input = process.argv[3];
 console.log('wat is going on ', answer);
+
 if (answer === 'read') {
   fs.readFile('./data.json', 'utf8', (err, data) => {
     if (err) throw err;
-    const gg = JSON.parse(data);
-    const gg2 = gg.notes;
-    for (const property in gg2) {
-      console.log(`${property}: ${gg2[property]}`);
+    const dataObj = JSON.parse(data);
+    const dataNotes = dataObj.notes;
+
+    for (const property in dataNotes) {
+      console.log(`${property}: ${dataNotes[property]}`);
     }
+  });
+}
+
+if (answer === 'create') {
+
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const dataObj = JSON.parse(data);
+
+    let currentId = dataObj.nextId;
+    dataObj.notes[currentId] = input;
+    currentId++;
+    dataObj.nextId = currentId;
+    const prettyObj = JSON.stringify(dataObj, null, 2);
+
+    fs.writeFile('data.json', prettyObj, err => {
+      if (err) throw err;
+    });
   });
 
 }
